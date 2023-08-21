0d6c70fd554da7c01db2915860edbb8485b92b9e
Arthur <diamondepic16@gmail.com>
2023-08-21 16:06:37 -0300

    Adicionando favicon

--- register.php
+++ register.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - About CPD</title>
+    <link rel="icon" href="./img/favicon.ico" type="image/x-icon">
 
     <link rel="stylesheet" href="./css/login.css">
     
@@ -18,7 +19,7 @@
                 <img src="./img/logo-login.png" alt="Logo Portal" class="login_logo">
                 <h1 class="login_title">REGISTRAR</h1>
 
-                <form action="#" method="POST">
+                <form action="verify_register.php" method="POST">
                     <label class="login_label">
                         <span>nome</span>
                         <input type="text" name="name" class="input">
@@ -33,7 +34,6 @@
                         <span>senha</span>
                         <input type="password" name="password" class="input">
                     </label>
-
                     <div class="wrapper">
                         <button type="submit" class="login_button" disabled>
                         <svg role="img" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-labelledby="forward" class="sc-hLseeU idnFbI sc-iAEyYk ciBkas"><title id="forward">forward icon</title><path d="M22.8011 14.75L14.2234 6.70971L16.0474 5L26.8695 15.1441C27.3732 15.6163 27.3732 16.3817 26.8695 16.8538L16.0474 26.998L14.2234 25.2883L22.7989 17.25H4.75V14.75H22.8011Z"></path>
