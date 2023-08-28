97d02470e9bdd7c9d884e2275f5d9154b910fac0
Arthur <diamondepic16@gmail.com>
2023-08-28 12:58:33 -0300

    Adição aside e Pagina index

---
+++ aside.php
@@ -0,0 +1,35 @@
+<nav>
+            <ul>
+                <li>
+                    <a href="#" class="logo">
+                        <img src="./img/logo-login.png" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fas fa-home"></i>
+                        <span class="nav-item">Home</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <i class="fas fa-solid fa-print"></i>
+                        <span class="nav-item">Impressoras</span>
+                    </a>
+                </li>
+                
+                <li>
+                    <a href="">
+                        <i class="fas fa-tasks"></i>
+                        <span class="nav-item">Diário</span>
+                    </a>
+                </li>
+                
+                <li>
+                    <a href="logout.php" class="logout">
+                        <i class="fas fa-sign-out-alt"></i>
+                        <span class="nav-item">Log out</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+include('user_verify.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt_BR">
+
+<head>
+    <meta charset="UTF-8" />
+    <title>About CPD</title>
+    <link rel="stylesheet" href="./css/index.css" />
+    <!-- Font Awesome Cdn Link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+    
+</head>
+
+<body>
+    <div class="container">
+        <?php
+            include('aside.php');
+        ?>
+
+       
+    </div>
+    <script src="https://kit.fontawesome.com/9516edc6f5.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
